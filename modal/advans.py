c508e70c26082540ff72a51927cd68a80dea0c86
1390aboalfazl <ablfzlr4@gmail.com>
2025-12-11 18:21:47 +0330

    a

--- modal/advans.py
+++ modal/advans.py
@@ -1,5 +1,6 @@
 def patrec(pro1,pro2,pro3,pro4):
-    open(pro1,'a').write('glo1')
-    open(pro2,'a').write('glo2')
-    open(pro3,'a').write('glo3')
-    open(pro4,'a').write('glo4')
+    open(pro1,'a').write('glo1:')
+    open(pro2,'a').write('glo2:')
+    open(pro3,'a').write('glo3:')
+    open(pro4,'a').write('glo4:')
+
